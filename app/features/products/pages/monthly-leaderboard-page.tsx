e56b3a7abcb427fc2c61158aec884c8e88978a41
m1nk1 <m1nk1@naver.com>
2025-04-29 22:28:24 +0900

    3.19 MetaArgs

--- app/features/products/pages/monthly-leaderboard-page.tsx
+++ app/features/products/pages/monthly-leaderboard-page.tsx
@@ -12,6 +12,22 @@ const paramsSchema = z.object({
   month: z.coerce.number(),
 });
 
+export const meta: Route.MetaFunction = ({ params }) => {
+  const date = DateTime.fromObject({
+    year: Number(params.year),
+    month: Number(params.month),
+  }).setZone("Asia/Seoul").setLocale("ko");
+
+  return [
+    {
+      title: `Best of ${date.toLocaleString({
+        month: "long",
+        year: "2-digit",
+      })} | wemake`,
+    },
+  ];
+};
+
 export const loader = ({ params }: Route.LoaderArgs) => {
   const { success, data: parseData } = paramsSchema.safeParse(params);
 
