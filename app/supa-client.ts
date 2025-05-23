02cfc8b74a86e4021ec2f417e3be8f5466cb9446
m1nk1 <m1nk1@naver.com>
2025-05-23 22:50:32 +0900

    Type Fest

--- app/supa-client.ts
+++ app/supa-client.ts
@@ -1,5 +1,25 @@
 import { createClient } from "@supabase/supabase-js";
-import { type Database } from "database.types";
+import { type MergeDeep, type SetFieldType, type SetNonNullable } from "type-fest";
+import { type Database as SupabaseDatabase } from "database.types";
+
+type Database = MergeDeep<
+  SupabaseDatabase,
+  {
+    public: {
+      Views: {
+        community_post_list_view: {
+          Row: SetFieldType<
+            SetNonNullable<
+              SupabaseDatabase["public"]["Views"]["community_post_list_view"]["Row"]
+            >,
+            "author_avatar",
+            string | null
+          >;
+        };
+      };
+    };
+  }
+>;
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
